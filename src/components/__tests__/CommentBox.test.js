25bc9a6e9f5eca0c8fb1bf49d808ad93659f19bf
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-04-13 14:00:33 +0530

    REACT_TESTING: Refactor tests using describe

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,28 +17,26 @@ it('has a text area and a button', () => {
   expect(wrapper.find('button').length).toEqual(1)
 })
 
-it('has a text area that users can type in', () => {
+describe('the text area', () => {
   const text = 'some test comment'
 
-  wrapper.find('textarea').simulate('change', { target: { value: text } })
+  beforeEach(() => {
+    wrapper.find('textarea').simulate('change', { target: { value: text } })
 
-  // setState is async. Which means React does not re-renders the component at the very instance but queues up on the setState call. So force update needed here.
+    // setState is async. Which means React does not re-renders the component at the very instance but queues up on the setState call. So force update needed here.
 
-  wrapper.update()
+    wrapper.update()
+  })
 
-  expect(wrapper.find('textarea').prop('value')).toEqual(text)
-})
+  it('has a text area that users can type in', () => {
+    expect(wrapper.find('textarea').prop('value')).toEqual(text)
+  })
 
-it('when form is submitted, text area gets emptied', () => {
-  wrapper
-    .find('textarea')
-    .simulate('change', { target: { value: 'new comment' } })
+  it('when form is submitted, text area gets emptied', () => {
+    wrapper.find('form').simulate('submit')
 
-  wrapper.update()
+    wrapper.update()
 
-  wrapper.find('form').simulate('submit')
-
-  wrapper.update()
-
-  expect(wrapper.find('textarea').prop('value')).toEqual('')
+    expect(wrapper.find('textarea').prop('value')).toEqual('')
+  })
 })
