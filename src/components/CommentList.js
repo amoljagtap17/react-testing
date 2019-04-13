8a4f47caf1e2881c3c65cd45831a2b73d26ef76a
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-04-13 19:39:30 +0530

    REACT_TESTING: Add Code for CommentList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { useStore } from 'hooks'
 
-const CommentList = () => <div>Comment List</div>
+const CommentList = () => {
+  const [{ comments }] = useStore()
+
+  const renderComments = () =>
+    comments.map(comment => <li key={comment}>{comment}</li>)
+
+  return (
+    <div>
+      <ul>{renderComments()}</ul>
+    </div>
+  )
+}
 
 export default CommentList
