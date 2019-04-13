9a74278daab0cbfec89e6fdc880781312903529f
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-04-13 22:48:15 +0530

    REACT_TESTING: Add Cheerio Queries

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -21,4 +21,7 @@ it('creates one LI per comment', () => {
   expect(wrapper.find('li').length).toEqual(2)
 })
 
-it('shows the text for each comment', () => {})
+it('shows the text for each comment', () => {
+  expect(wrapper.render().text()).toContain('Comment 1')
+  expect(wrapper.render().text()).toContain('Comment 2')
+})
